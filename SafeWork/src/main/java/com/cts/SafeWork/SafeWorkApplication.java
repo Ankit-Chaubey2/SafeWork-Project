ad13485d87b4d9abda07addabaa46fe97dd7a83e
Ankit-chaubey1 <chaubeyankit927@gmail.com>
2026-03-05 15:54:24 +0530

    ok

---
+++ SafeWork/src/main/java/com/cts/SafeWork/SafeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.cts.SafeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeWorkApplication.class, args);
+	}
+
+}
---
+++ SafeWork/src/test/java/com/cts/SafeWork/SafeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.SafeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
